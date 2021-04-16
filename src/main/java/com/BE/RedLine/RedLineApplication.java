edecf8883cf0d9f79801a15a0d6992b35936d779
Schoony415 <joshua.schoonover@austincc.edu>
2021-04-16 11:42:37 -0500

    cors

--- src/main/java/com/BE/RedLine/RedLineApplication.java
+++ src/main/java/com/BE/RedLine/RedLineApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class RedLineApplication {
 	private String ExpectedURL = "http://localhost:3000";
+	private String outsideIP = "198.213.89.195";
 	public static void main(String[] args) {
 		SpringApplication.run(RedLineApplication.class, args);
 	}
@@ -23,18 +24,22 @@ public class RedLineApplication {
 				registry.addMapping("/requests")
 						.allowedMethods("GET")
 						.allowedOrigins(ExpectedURL)
+						.allowedOrigins(outsideIP)
 				;
 				registry.addMapping("/requests/{id}")
 						.allowedMethods("GET","PATCH")
 						.allowedOrigins(ExpectedURL)
+						.allowedOrigins(outsideIP)
 				;
 				registry.addMapping("/responder/{name}")
 						.allowedMethods("GET")
 						.allowedOrigins(ExpectedURL)
+						.allowedOrigins(outsideIP)
 				;
 				registry.addMapping("/nineline")
 						.allowedMethods("POST")
 						.allowedOrigins(ExpectedURL)
+						.allowedOrigins(outsideIP)
 				;
 			}
 		};
