2eb5db0d32468d709610fb4e5ed656c5883cf199
Schoony415 <joshua.schoonover@austincc.edu>
2021-04-16 09:15:39 -0500

    base endpoint

--- BackEnd/RedLine/src/main/java/com/BE/RedLine/RedLineApplication.java
+++ BackEnd/RedLine/src/main/java/com/BE/RedLine/RedLineApplication.java
@@ -3,6 +3,7 @@ package com.BE.RedLine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
