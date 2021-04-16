bf91558f360b25578ddc81009253fa427a999969
Schoony415 <joshua.schoonover@austincc.edu>
2021-04-16 15:24:56 -0500

    cors

--- src/main/java/com/BE/RedLine/BaseEndpoint.java
+++ src/main/java/com/BE/RedLine/BaseEndpoint.java
@@ -1,11 +1,12 @@
 package com.BE.RedLine;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BaseEndpoint {
-    @GetMapping("")
+    //@GetMapping("")
+    @CrossOrigin
+    @RequestMapping(method= RequestMethod.GET, path = "")
     public String main(){
         return "<html><body><h1 color='#ff0000' >This endpoint works! welcome to online hosting! have a good day</h1></body></html>";
     }
--- src/main/java/com/BE/RedLine/RedLineApplication.java
+++ src/main/java/com/BE/RedLine/RedLineApplication.java
@@ -17,10 +17,10 @@ import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 public class RedLineApplication {
-	private String ExpectedURL = "http://localhost:3000";
-	private String internalIP = "127.0.0.1:3000";
-	private String outsideIP = "198.213.89.195"; //ip of school
-	private String outsideIP2 = "198.213.89.145"; //ip of school 2
+//	private String ExpectedURL = "http://localhost:3000";
+//	private String internalIP = "127.0.0.1:3000";
+//	private String outsideIP = "198.213.89.195"; //ip of school
+//	private String outsideIP2 = "198.213.89.145"; //ip of school 2
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedLineApplication.class, args);
@@ -79,48 +79,48 @@ public class RedLineApplication {
 //	}
 }
 
-
-@Configuration
-@EnableWebMvc
-class WebConfig implements Filter,WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
-	}
-
-	@Override
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
-		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
-		System.out.println("WebConfig; "+request.getRequestURI());
-		response.setHeader("Access-Control-Allow-Origin", "https://frontportafolio.herokuapp.com");
-		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
-		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Expose-Headers", "Authorization");
-		response.addHeader("Access-Control-Expose-Headers", "responseType");
-		response.addHeader("Access-Control-Expose-Headers", "observe");
-		System.out.println("Request Method: "+request.getMethod());
-		if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
-			try {
-				chain.doFilter(req, res);
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
-		} else {
-			System.out.println("Pre-flight");
-			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"+"Authorization, content-type," +
-					"USERID"+"ROLE"+
-					"access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
-			response.setStatus(HttpServletResponse.SC_OK);
-		}
-
-	}
-
-}
-
+//
+//@Configuration
+//@EnableWebMvc
+//class WebConfig implements Filter,WebMvcConfigurer {
+//
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**");
+//	}
+//
+//	@Override
+//	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+//		HttpServletResponse response = (HttpServletResponse) res;
+//		HttpServletRequest request = (HttpServletRequest) req;
+//		System.out.println("WebConfig; "+request.getRequestURI());
+//		response.setHeader("Access-Control-Allow-Origin", "https://frontportafolio.herokuapp.com");
+//		response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+//		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+//		response.setHeader("Access-Control-Max-Age", "3600");
+//		response.setHeader("Access-Control-Allow-Credentials", "true");
+//		response.setHeader("Access-Control-Expose-Headers", "Authorization");
+//		response.addHeader("Access-Control-Expose-Headers", "responseType");
+//		response.addHeader("Access-Control-Expose-Headers", "observe");
+//		System.out.println("Request Method: "+request.getMethod());
+//		if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+//			try {
+//				chain.doFilter(req, res);
+//			} catch(Exception e) {
+//				e.printStackTrace();
+//			}
+//		} else {
+//			System.out.println("Pre-flight");
+//			response.setHeader("Access-Control-Allow-Origin", "*");
+//			response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+//			response.setHeader("Access-Control-Max-Age", "3600");
+//			response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"+"Authorization, content-type," +
+//					"USERID"+"ROLE"+
+//					"access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
+//			response.setStatus(HttpServletResponse.SC_OK);
+//		}
+//
+//	}
+//
+//}
+//
